9321f297c079f60c99d66ec0124e2007d254027d
Karolina Piróg <karolina.swieca@wp.pl>
2018-04-05 13:12:16 +0200

    add stript.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var a = 3;
+var b = 4;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+console.log (value);
+
+var value = value >= 0 ? 'wynik dodatni' : 'wynik ujemny';
+
+console.log(value);
+
+var value = value = 0 ? 'wynik równy 0' : 'wynik nierówny 0';
+
+console.log(value);
+
+
+

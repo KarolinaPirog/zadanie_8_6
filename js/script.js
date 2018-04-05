1cda3f30aebf1405514e4315ae374b702fffc4cb
Karolina Piróg <karolina.swieca@wp.pl>
2018-04-05 13:29:55 +0200

    change js file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-var a = 3;
-var b = 4;
+var a = prompt('podaj wartość a');
+var b = prompt('podaj wartość b');
 var value = (a * a) - (2 * a * b) - (b * b);
 
 console.log (value);
 
-var value = value >= 0 ? 'wynik dodatni' : 'wynik ujemny';
+console.log(value >= 0 ? 'wynik dodatni' : 'wynik ujemny');
 
-console.log(value);
-
-var value = value = 0 ? 'wynik równy 0' : 'wynik nierówny 0';
-
-console.log(value);
-
-
-
+console.log(value == 0 ? 'wynik równy 0' : 'wynik nierówny 0');
